94873e124bc62a866a457908f555c8b82abdf72f
Ishan Arya <ishanarya0@gmail.com>
2020-11-05 18:54:15 +0530

    Iterative Postorder Traversal Updated
    Reference https://www.geeksforgeeks.org/iterative-postorder-traversal-using-stack/?ref=rp

--- Trees/1_Binary_Tree.cpp
+++ Trees/1_Binary_Tree.cpp
@@ -228,6 +228,7 @@ void Tree::printPreorderIter(struct node* curr){
 	
 }
 
+/*
 void Tree::printPostorderIter(struct node* curr){
 	stack <node*> s;
 	node* temp = curr;
@@ -258,6 +259,53 @@ void Tree::printPostorderIter(struct node* curr){
 		}	
 	}
 	return;
+}*/
+
+/*
+1.1 Create an empty stack
+2.1 Do following while root is not NULL
+	a) Push root's right child and then root to stack
+	b) Set root as root's left child
+2.2 Pop an item from stack and set it as root
+	a) If the popped item has a right child and the right child is at top of stack
+		, then remove the right child from stack, push the root back
+		and set root as root's right child.
+	b) Else print root's data and set root as NULL;
+2.3 Repeat steps 2.1 and 2.2 while stack is not empty	
+*/
+
+void Tree::printPostorderIter(struct node* root){
+		stack<struct node*> s;
+		if(root == NULL)
+			return;
+		do{
+			while(root != NULL){
+				if(root->right){		
+					s.push(root->right);
+				}
+				s.push(root);
+				
+				root = root->left;
+			}
+			
+			root = s.top();
+			s.pop();
+			if(s.empty()){
+				cout<<root->data;
+				return;
+			}
+			
+			if((root->right )&& (root->right == s.top())){
+				s.pop();
+				s.push(root);
+				root = root->right;
+			}
+			else{
+				cout<<root->data<<" ";
+				root = NULL;
+			}
+			
+		}while(!s.empty());
 }
 
 /////////////////////////////////
